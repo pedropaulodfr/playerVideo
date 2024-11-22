e673f4b2001d2788f09513456ecf5bdddeb523a7
Pedro Paulo <pedropaulodfr@outlook.com>
2024-11-21 22:41:19 -0300

    Alterando novela para Flor do Caribe

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -25,7 +25,7 @@ const Sidebar: React.FC = () => {
     localStorage.removeItem("ultimoVideo")
     localStorage.setItem("ultimaNovela", event.target.value)
     setUltimaNovelaSelecionada(event.target.value)
-    onChangeNovelaAtual(event.target.value == "Flor do Caribe" ? Flor_do_Caribe : Flor_do_Caribe)
+    onChangeNovelaAtual(Flor_do_Caribe)
   }
 
   return (
