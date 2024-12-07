debc907252db1e7896801c069b43f9cf5e0cf77e
Pedro Paulo <pedropaulodfr@outlook.com>
2024-12-07 14:59:35 -0300

    Definindo parametrizações

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -25,6 +25,11 @@ const Sidebar: React.FC = () => {
     localStorage.setItem("ultimaNovela", event.target.value)
   }
 
+  const listaNovelas = [
+    {titulo: "Flor do Caribe", url: "https://playervideo.onrender.com/"},
+    {titulo: "Totalmente Demais", url: "https://totalmentedemais.onrender.com/"}
+  ]
+
   return (
     <div className={isOpen ? styles.sidebarOpen : styles.sidebarClosed}>
       <button onClick={toggleSidebar} className={styles.toggleButton}>
@@ -33,20 +38,13 @@ const Sidebar: React.FC = () => {
       <nav className={styles.menu}>
         <ul className={isOpen ? styles.menuList : styles.menuListClosed}>
           <h1 style={{ fontSize: "1.3em", fontWeight: "bold" }}>Lista de Novelas<CiBoxList onClick={toggleNovelas} size={30} style={{cursor: "pointer"}}/></h1>
-          {/* {listNovelasOpen &&
+          {listNovelasOpen &&
             listaNovelas.map((novela, key) => (
-              <label key={key} className={`${styles.label}`}>
-                <input className={`${styles.radio}`}
-                  type="radio"
-                  value={novela}
-                  checked={novela == novelaSelecionada}
-                  onChange={onChangeNovela}
-                >
-                </input>
-                {novela}
-              </label>
+              <Link href={novela.url} key={key} style={{ display: 'block', marginBottom: '8px' }}>
+                {novela.titulo}
+              </Link>
             ))
-          } */}
+          }
         </ul>
         <ul className={isOpen ? styles.menuList : styles.menuListClosed}>
           <h1 style={{ fontSize: "1.3em", fontWeight: "bold" }}>Lista de Capítulos</h1>
---
+++ src/helpers/functions.js
@@ -0,0 +1,55 @@
+const salvarParametros = (propriedade, valor) => {
+    var propriedade = String(propriedade)
+    var valor = String(valor)
+    var parametros = localStorage.getItem("params")
+
+    if (parametros != undefined) {
+        let parametrosJSON = JSON.parse(parametros); // Converte a string em um array manipulável
+        
+        // Verifica se já existe um objeto com a propriedade
+        const propriedadeIndex = parametrosJSON.findIndex(param => param.hasOwnProperty(propriedade));
+  
+        if (propriedadeIndex !== -1) {
+            // Atualiza o valor da propriedade, se já existir
+            parametrosJSON[propriedadeIndex][propriedade] = valor;
+        } else {
+            // Adiciona um novo objeto caso não exista
+            parametrosJSON.push({ [propriedade]: valor });
+        }
+  
+        // Atualiza o localStorage com o array modificado
+        localStorage.setItem("params", JSON.stringify(parametrosJSON));
+    }
+}
+
+const pegarParametro = (parametro) => {
+    var parametro = String(parametro)
+
+    var parametros = localStorage.getItem("params")
+
+    if(parametros != undefined) {
+        let parametrosJSON = JSON.parse(parametros)
+
+        const parametroIndex = parametrosJSON.findIndex(param => param.hasOwnProperty(parametro))
+
+        if(parametroIndex !== -1) {
+            return parametrosJSON[parametroIndex][parametro]
+        } else {
+            return null
+        }
+    }
+}
+
+const lightColors = () => {
+    return [
+        {"primaria": "#EDF0F1", "secundaria": "#BEC3C7"}
+    ]
+}
+
+const darkColors = () => {
+    return [
+        {"primaria": "#324456", "secundaria": "#394F64"}
+    ]
+}
+
+export default {salvarParametros, pegarParametro, lightColors, darkColors}
